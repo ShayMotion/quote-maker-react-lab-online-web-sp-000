2c887ffd3af78e6f0dcf0419043f12d5aa46bc03
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 17:00:03 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,19 +1,25 @@
-// TODO: Create action creators as defined in tests
-export const addQuote = quote => {
+export function addQuote(quote) {
   return {
     type: 'ADD_QUOTE',
-    quote
+    quote: Object.assign({}, quote, {votes: 0})
   }
 }
 
-export const removeQuote = quoteID => {
+export function removeQuote(quoteId) {
   return {
     type: 'REMOVE_QUOTE',
     quoteId
   }
 }
 
-export const upvoteQuote = quoteId => {
+export function upvoteQuote(quoteId) {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId
+  }
+}
+
+export function downvoteQuote(quoteId) {
   return {
     type: 'DOWNVOTE_QUOTE',
     quoteId
